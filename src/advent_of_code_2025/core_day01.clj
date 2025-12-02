4a5def9f23d843289cf197fc4f439723f4df72e1
Nandaja Varma <nandaja.varma@gmail.com>
2025-12-02 09:03:10 +0100

    fix test

--- src/advent_of_code_2025/core_day01.clj
+++ src/advent_of_code_2025/core_day01.clj
@@ -70,22 +70,3 @@
 
 
 
-;; unittests
-(deftest test-turnRight
- (testing "Turn right wraps around at 100"
-     (is (= 0 (turnRight 99 1)))
-     (is (= 0 (turnRight 95 5)))
- )
-)
-(deftest test-turnLeft
- (testing "Turn left wraps around at 0"
-     (is (= 99 (turnLeft 0 1)))
-     (is (= 95 (turnLeft 5 10)))
-)
-)
-
-(deftest test-rotate-dials
- (testing "Counts zero crossings"
-     (is (= 3 (rotate-dials 10 ["L68" "L30" "R48" "L5" "R60" "L55" "L1" "L99" "R14" "L82"])))
-     )
-  )
--- test/advent_of_code_2025/core_test.clj
+++ test/advent_of_code_2025/core_test.clj
@@ -2,6 +2,31 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2025.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+;; day01
+(deftest test-turnRight
+ (testing "Turn right wraps around at 100"
+     (is (= 0 (turnRight 99 1)))
+     (is (= 0 (turnRight 95 5)))
+ )
+)
+(deftest test-turnLeft
+ (testing "Turn left wraps around at 0"
+     (is (= 99 (turnLeft 0 1)))
+     (is (= 95 (turnLeft 5 10)))
+)
+)
+
+(deftest test-rotate-dials
+ (testing "Counts zero crossings"
+     (is (= 3 (rotate-dials 50 ["L68" "L30" "R48" "L5" "R60" "L55" "L1" "L99" "R14" "L82"])))
+     )
+  )
+
+(deftest test-rotate-dials-part-b
+ (testing "Counts zero crossings"
+     (is (= 6 (rotate-dials-part-b 50 ["L68" "L30" "R48" "L5" "R60" "L55" "L1" "L99" "R14" "L82"])))
+     (is (= 10 (rotate-dials-part-b 50 ["R1000"])))
+     )
+  )
